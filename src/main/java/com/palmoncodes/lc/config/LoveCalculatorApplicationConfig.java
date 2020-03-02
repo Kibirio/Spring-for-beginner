edc3839ec4e723bdd52f3a19ca2eb1980217cd5c
kibirio <ruiruharrison047@gmail.com>
2020-03-02 14:27:34 +0300

    ny first commit

---
+++ src/main/java/com/palmoncodes/lc/config/LoveCalculatorApplicationConfig.java
@@ -0,0 +1,10 @@
+package com.palmoncodes.lc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.palmoncodes.lc.controllers")
+public class LoveCalculatorApplicationConfig {
+
+}
---
+++ src/main/java/com/palmoncodes/lc/controllers/TestController.java
@@ -0,0 +1,17 @@
+package com.palmoncodes.lc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/test")
+	@ResponseBody
+	public String sayHello()
+	{
+		return "hi! this is test controller";
+	}
+
+}
