63b0f4e4f6c1a18d71d9e9f8d378571079f80954
kibirio <ruiruharrison047@gmail.com>
2020-03-03 13:55:08 +0300

    modified files

---
+++ src/main/java/com/palmoncodes/lovecalc/TestController.java
@@ -0,0 +1,15 @@
+package com.palmoncodes.lovecalc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/welcome")
+	public String testController()
+	{
+		return "hello";
+	}
+
+}
--- src/main/webapp/WEB-INF/view/hello.jsp
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -4,9 +4,9 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>palmoncodes</title>
 </head>
 <body>
-
+<h1 align="center">WELCOME TO PALMONCODES CAFE</h1>
 </body>
 </html>
